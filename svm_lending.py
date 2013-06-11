d283ce63edb6106f06c51b6e478fa3fa1c49a443
Pavel Machalek <pavel@TCC-PAVEL.local>
2013-06-11 11:39:34 -0700

    Finally working

--- svm_lending.py
+++ svm_lending.py
@@ -1,3 +1,4 @@
+#!/export/disk0/wb/python2.6/bin/python
 from sklearn import (svm, preprocessing,
                      grid_search, metrics)
 import pandas as pd
@@ -45,20 +46,20 @@ def prepare_data_for_year(training, target_y, def_scaler=None):
     return X_scaled, status, scaler
 
 def main():
-training = pd.read_csv("LoanStatsNew.csv")
+    training = pd.read_csv("LoanStatsNew.csv")
 
-X_scaled_2009, status_2009, scaler_2009 = prepare_data_for_year(training, 2009, def_scaler=None)
+    X_scaled, status, scaler_init = prepare_data_for_year(training, 2009, def_scaler=None)
 #X_scaled_2012, status_2012, scaler_2012 = prepare_data_for_year(training, 2012, def_scaler=None)
-X_scaled_2013, status_2013, _ = prepare_data_for_year(training, 2013, def_scaler=scaler_2009)
+    X_scaled_2013, status_2013, _ = prepare_data_for_year(training, 2013, def_scaler=scaler_init)
 
 
 #Train on a grid search for gamma and C
-parameters = [{'C': [0.1,1, 10, 100, 1000,10000],
-               'gamma': [10,1,0.1,0.01, 0.001],
-               'kernel': ['poly','rbf'], 'degree': [2, 3]}]
-classifier = grid_search.GridSearchCV(svm.SVC(C=1), parameters,
+    parameters = [{'C': [0.1, 1, 10, 100, 1000],
+                   'gamma': [0.1,0.01, 0.001],
+                   'kernel': ['poly','rbf'], 'degree': [2, 3]}]
+    classifier = grid_search.GridSearchCV(svm.SVC(C=1), parameters,
                                       verbose=3, n_jobs=4)
-classifier.fit(X_scaled_2012, status_2012)
+    classifier.fit(X_scaled, status)
 
 #clf = svm.LinearSVC().fit(X_scaled, status)
 #C = 100.0  # SVM regularization parameter
@@ -68,9 +69,14 @@ classifier.fit(X_scaled_2012, status_2012)
 
 
 #Predict
-predict_2013 = classifier.predict(X_scaled_2013)
+    predict_2013 = classifier.predict(X_scaled_2013)
+    print metrics.classification_report(status_2013, predict_2013)
 #Report
 
 #Cross validate
 #scores = cross_validation.cross_val_score(clf, iris.data, iris.target, cv=5)
 #Fit test data
+    return 0
+
+if __name__ == '__main__':
+    main()
